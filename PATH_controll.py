27d5515af92670f5eb271fe30607bb2075836842
Ryan B <klhong124@gmail.com>
2019-03-06 19:35:37 +0800

    update

--- PATH_controll.py
+++ PATH_controll.py
@@ -1,6 +1,7 @@
-pathbase = "C:\\Users\\Hong\\GitHub\\Business-Logistics-System\\public\\"
-csvname = "test.output.csv"
+pathbase = "C:\\Users\\Hong\\GitHub\\Business-Logistics-System\\public\\uploads\\"
+#csvname = "test.output.csv"
 #csvname = "Customer.csv"
-csvpath = pathbase + "uploads\\" + csvname
+csvname = "data.csv"
+csvpath = pathbase + csvname
 jsonpath = pathbase + "csv_log.json"
 reviewpath = "json_read.json"
